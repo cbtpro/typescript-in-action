6bc5ab8469bd3dd8968b5552ce97596a4979562f
chenbitao <cbtpro@gmail.com>
2020-04-07 09:49:09 +0800

    typescript语法

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./datatype");
+var hello = 'Hello Typescript!';
+document.querySelectorAll('.app')[0].innerHTML = hello;
