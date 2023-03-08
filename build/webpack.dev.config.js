a2c06ef04973c9cc2402622200bba256e81ec03d
陈碧滔 <cbtpro@163.com>
2023-03-08 18:04:09 +0800

    feat: 重构webpack编译脚本；增加清除dist配置

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,10 +1,20 @@
-const webpack = require('webpack')
+const { DefinePlugin } = require('webpack');
 
 module.exports = {
-    devtool: 'cheap-module-source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            BASE_URL: JSON.stringify('//dev.example.com')
-        })
-    ]
-}
+  // 控制生成sourcemap https://webpack.docschina.org/configuration/devtool/#devtool
+  devtool: 'eval-source-map',
+  devServer: {
+    client: {
+      // 开启客户端错误遮罩 https://webpack.docschina.org/configuration/dev-server/#overlay
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
+  plugins: [
+    new DefinePlugin({
+      BASE_URL: JSON.stringify('//dev.example.com'),
+    }),
+  ],
+};
--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,11 +1,15 @@
-const webpack = require('webpack')
-const { CleanWebpackPlugin }  = require('clean-webpack-plugin')
+const { DefinePlugin } = require('webpack');
+const { resolve } = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    plugins: [
-        new webpack.DefinePlugin({
-            BASE_URL: JSON.stringify('//www.example.com')
-        }),
-        new CleanWebpackPlugin()
-    ]
-}
+  output: {
+    path: resolve(process.cwd(), 'dist'),
+  },
+  plugins: [
+    new DefinePlugin({
+      BASE_URL: JSON.stringify('//www.example.com'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
