6833151834e5ed7e4425f8baba9eeffc7d27a417
陈碧滔 <cbtpro@163.com>
2023-03-08 18:05:22 +0800

    feat: 增加函数重载的两种方式

--- src/index.ts
+++ src/index.ts
@@ -71,3 +71,67 @@ function calculateArea2(shape: Shape2) {
     return shape.width * shape.width;
   }
 }
+
+/** ts中函数重载实现 */
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+
+function add(a: number | string, b: number | string) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  } else if (typeof a === 'string' && typeof b === 'string') {
+    return a + b;
+  }
+  return [a, b].join(' ');
+}
+
+console.log('add(1, 2) = ', add(1, 2));
+console.log('add(\'hello\', \'world\') = ', add('hello', 'world'));
+console.log('add(\'age\', \'18\') = ', add('age', 18));
+console.log('add(2023, \'年\') = ', add(2023, '年'));
+
+type customArray = (string | number | boolean)[];
+function len(s: string): number;
+function len(arr: customArray): number;
+function len<T extends customArray>(x: T) {
+  return x.length;
+}
+console.log('len(\'hello world\')', len('hello world'));
+console.log('len([1, 2, 3])', len([1, 2, 3, 'hello world', true]));
+
+/** class继承中的函数重载 */
+class Person {
+  name: string = '干饭人';
+  constructor(name: string) {
+    this.name = name;
+  }
+  eat(): void {
+    console.log(this.name + '饿了就吃');
+  }
+}
+
+class Student extends Person {
+  rollNumber: number = 0;
+  constructor(rollNumber: number, name: string) {
+    super(name);
+    // 因为有了super(name)和person中的构造函数，所以不用再次赋值name
+    // this.name = name;
+    this.rollNumber = rollNumber;
+  }
+  displayInformation(): void {
+    console.log(`${this.name} 滚动${this.rollNumber}圈`);
+  }
+  override eat(): void {
+    console.log(this.name, '午餐时吃饭');
+  }
+}
+
+const student = new Student(10, '小明');
+student.displayInformation();
+student.eat();
+/**
+ * 重写函数时override可以省略
+ */
+
